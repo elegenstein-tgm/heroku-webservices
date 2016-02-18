5211341f8a100933e8765aa5fa00626b85235f74
ArchX1-erik <elegenstein@student.tgm.ac.at>
2016-02-19 00:33:40 +0100

    suck everything

--- src/main/java/dez09/ebraendli/UserDB.java
+++ src/main/java/dez09/ebraendli/UserDB.java
@@ -4,10 +4,9 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
 
 /**
  * Created by fusions on 18.02.16.
@@ -19,19 +18,31 @@ public class UserDB {
 
     /* Due to sqlite shittig errors it is not possible to connect
     *see*/
+
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
     public static String getUser(String uname, String upwd) {
         //LOG.info("getting name : " + this.name);
         try {
-            Context ctx = new InitialContext();
-            DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dez09");
-            Connection conn = ds.getConnection();
+//            Context ctx = new InitialContext();
+//            DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dez09");
+            Connection conn = getConnection();
             Statement stat = conn.createStatement();
             ResultSet rs = stat.executeQuery("select uemail from user WHERE uname LIKE '" + uname + "' AND upwd LIKE '" + upwd + "'");
             return rs.getString(1);
         } catch (SQLException se) {
             se.printStackTrace();
-        } catch (NamingException ne) {
-            ne.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
         return null;
     }
@@ -39,9 +50,9 @@ public class UserDB {
         Context ctx = null;
 
         try {
-            ctx = new InitialContext();
-            DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dez09");
-            Connection conn = ds.getConnection();
+            //ctx = new InitialContext();
+            //DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dez09");
+            Connection conn = getConnection();
             Statement stat = conn.createStatement();
             stat.execute("INSERT INTO user VALUES ('" + uemail+"','" +uname+ "','"+upwd+"')");
         } catch (NamingException e) {
